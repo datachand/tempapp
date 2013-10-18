0377b0eae6807b0e552001b7608ea6803cbf6702
Data <data.chand@gmail.com>
2013-10-18 15:09:55 +0530

    Just Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tempapp::Application.config.secret_key_base = '360622600381bb00638fa98349181b9df790601e6ed789e7ecf221a42321b0f56fadf11de2ebf74d9018a440f365903e19facc0c775300c967ccef57f1875726'
